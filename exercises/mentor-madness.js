400698e12d904762025880aad94a82a5a3c9c176
Bryan Newby <brnewby602@gmail.com>
2016-03-21 23:17:20 -0700

    Resolve mentor-madness and single-threaded questions, add index.html to test mentor-madness in browser

--- exercises/mentor-madness.js
+++ exercises/mentor-madness.js
@@ -13,12 +13,20 @@ var foo = {
 var findMentor = foo.bar.getMentor;
 
 // What would the following log?
+   // 'Beth'
 // What binding pattern(s) cause this?
+   // method invocation
 foo.bar.getMentor();
+
+// this will console log undefined in node, but will log correctly in a browser
+// this is free form function invocation
 findMentor();
 
 // How would you modify the following invocations of findMentor 
 // to log each of the mentor's names?
-findMentor(); // Should log 'Zach'
-findMentor(); // Should log 'Sunny'
-findMentor(); // Should log 'Beth'
+findMentor(); // Should log 'Zach'  -- does log Zach in the browser.  
+findMentor.call(foo); // Should log 'Sunny'
+
+findMentor.call(foo.bar); // Should log 'Beth'
+
+console.log(global);
--- exercises/single-threaded.js
+++ exercises/single-threaded.js
@@ -7,7 +7,7 @@ var Project = function (title, link, stack) {
 Project.prototype.describeStack = function() {
   this.stack.forEach(function(framework) {
     console.log(this.title + ' was built with ' + framework);
-  });
+  }.bind(this));
 };
 
 var myTitle = 'Sewing with JavaScript';
@@ -16,6 +16,7 @@ var myStack = ['Backbone', 'Node', 'MongoDB'];
 var sewingWithJS = new Project(myTitle, myLink, myStack);
 
 // What would the following log?
+
 // What binding pattern(s) cause this?
 // How would you modify the .describeStack method to achieve the expected results?
 sewingWithJS.describeStack();
