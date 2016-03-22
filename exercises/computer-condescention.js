c83659cd4faaec33ffe74f5a6ffc22bcb4392777
Bryan Newby <brnewby602@gmail.com>
2016-03-21 22:21:32 -0700

    Add question answers and resolve 'this' problem with context variable

--- exercises/computer-condescention.js
+++ exercises/computer-condescention.js
@@ -3,14 +3,24 @@ var computer = {
   owner: 'Dave',
   condescend: function() {
     console.log('Processing...');
+    var context = this;
     setTimeout(function() {
-      console.log('I\'m sorry, ' + this.owner + ', I can\'t let you do that.');
+      console.log('I\'m sorry, ' + context.owner + ', I can\'t let you do that.');
     }, 1000);
   }
 };
 
-// What would the following log?
+// What would the following log?  
+   // 'Processing'
+   // 'I'm sorry, undefined, I can't let you do that.'
+
 // What binding pattern causes this?
+  // when setTimeout is executed, it is being called from window, not computer object.  
+  // free form method invocation.
 computer.condescend();
 
 // How would we modify the .condescend method to achieve the expected output?
+
+   // could create a variable inside condescend that stores 'this', and then 
+   // reference the variable, or bind the anonymous function to this
+   //  function(){...}.bind(this)
